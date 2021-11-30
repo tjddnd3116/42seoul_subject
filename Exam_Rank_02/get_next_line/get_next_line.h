b7e2ada44f1542a89affa6b1f2bd3485c04724af
Sungwoong Oum <soum@c2r10s7.42seoul.kr>
2021-11-30 12:36:31 +0900

    exam02_get_next_line

--- Exam_Rank_02/get_next_line/get_next_line.h
+++ Exam_Rank_02/get_next_line/get_next_line.h
@@ -1,8 +1,12 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 
--- Exam_Rank_02/get_next_line/main.c
+++ Exam_Rank_02/get_next_line/main.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
 
-int main(void)
+int	main(void)
 {
-	int		r;
-	char	*line;
 	int		fd;
+	char	*line;
 
 	fd = open("test.txt", O_RDONLY);
 	line = get_next_line(fd);
-	while (line != NULL)
-	{	
-		printf("%s\n", line);
+	while (line)
+	{
+		printf("%s", line);
 		free(line);
 		line = get_next_line(fd);
 	}
 	printf("%s", line);
 	free(line);
-	line = NULL;
+	system("leaks a.out");
+	return (0);
 }
