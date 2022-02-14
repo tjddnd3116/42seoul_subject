89fd966ec26ca1ade0c5239c6138825865b48f4d
엄성웅 <tjddnd3116@gmail.com>
2022-02-14 11:21:35 +0900

    cpp_module_00/ex00

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
-#include <string>
+
+class awake {
+	public:
+		awake() {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		}
+		awake(int argc, char **argv) {
+			int i;
+
+			i = 1;
+			if (argc == 1)
+				std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+			else {
+				while (i < argc) {
+					std::cout << argv[i];
+					i++;
+				}
+				std::cout << std::endl;
+			}
+		}
+};
 
 int main(int argc, char **argv)
 {
+	awake megaphone(argc, argv);
 	return (0);
 }
