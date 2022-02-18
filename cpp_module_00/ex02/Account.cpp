778b279aa7135040d0bd510af970b61bddbf87ff
엄성웅 <tjddnd3116@gmail.com>
2022-02-18 20:48:22 +0900

    cpp_module_00

---
+++ cpp_module_00/ex02/Account.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soum <soum@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/18 19:07:57 by soum              #+#    #+#             */
+/*   Updated: 2022/02/18 19:50:00 by soum             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+// static int getNbAccounts(void)
+// {
+//   return (0);
+// }
+//
+// static int getTotalAmount(void)
+// {
+//   return (0);
+// }
+//
+// static int getNbDeposits(void)
+// {
+//   return (0);
+// }
+//
+// static int getNbWithdrawals(void)
+// {
+//   return (0);
+// }
+//
+// static void displayAccountsInfos(void)
+// {
+//
+// }
+
+void makeDeposit(int deposit)
+{
+	(void)deposit;
+}
+
+bool makeWithdrawal(int withdrawal)
+{
+	(void)withdrawal;
+	return (1);
+}
+
+int	checkAmount(void)
+{
+	return (0);
+}
+
+void	displayStatus(void)
+{
+
+}
--- cpp_module_00/ex02/test.cpp
+++ cpp_module_00/ex02/test.cpp
@@ -6,7 +6,7 @@
 
 int		main (void) {
 
-	typeef std::vector<Account::t>	accounts_t;
+	typedef std::vector<Account::t>	accounts_t;
 	typedef std::vector<int>								  ints_t;
 	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
 
