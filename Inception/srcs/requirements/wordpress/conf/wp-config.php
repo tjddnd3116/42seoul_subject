c77fb3ccd2c58e76e0bac70e6df90f585aa8aefa
tjddnd3116 <tjddnd3116@gmail.com>
2022-05-06 22:42:47 +0900

    inception/docker-compose

--- Inception/srcs/requirements/wordpress/conf/wp-config.php
+++ Inception/srcs/requirements/wordpress/conf/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'soum' );
 define( 'DB_PASSWORD', '12345' );
 
 /** Database hostname */
-define( 'DB_HOST', '172.17.0.2' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
