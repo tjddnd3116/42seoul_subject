f232a9c1050856b6cab86541a35ff8576d0f4e75
tjddnd3116 <tjddnd3116@gmail.com>
2022-09-14 00:22:41 +0900

    fix :	wordpress: 	    if already installed wordpress don't install wordpress and 	    create user 	mariadb 	    only connect to wordpress with mariadb user todo :     want to use only 'wordpress' in config mariadb user to connect ip     address so study it and fix it
        make makefile
    
    add wordpress::dockerfile
        if wp --allow-root core is-installed; then
    
        mariadb::conf.sh
        '$MYSQL_USER'@'wordpress.srcs_inception_net'

--- Inception2/srcs/requirements/mariadb/tools/conf.sh
+++ Inception2/srcs/requirements/mariadb/tools/conf.sh
@@ -3,11 +3,6 @@ perl -p -i -e '$.==19 and print "port = 3306\n"' /etc/mysql/mariadb.conf.d/50-se
 perl -pi -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 echo "CREATE DATABASE $MYSQL_DATABASE;\
-        GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';\
+		GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'wordpress.srcs_inception_net' IDENTIFIED BY '$MYSQL_PASSWORD';\
         FLUSH PRIVILEGES;\
 		ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" > mariadb.sh
-
-if [ ! -z ${MYSQL_DATABASE} ]
-then
-	unset MYSQL_DATABASE MYSQL_USER MYSQL_PASSWORD MYSQL_ROOT MYSQL_ROOT_PASSWORD
-fi
--- Inception2/srcs/requirements/wordpress/tools/conf.sh
+++ Inception2/srcs/requirements/wordpress/tools/conf.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
 mkdir -p /var/run/php
+# nginx와의 통신을 위해 php-fpm listen port 변경
 perl -pi -e 's/\/run\/php\/php7.3-fpm.sock/$ENV{"WP_PORT"}/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+# 워드프레스의 중복 설치 및 중복 유저추가를 방지
 if [ ! -d "/var/www/wordpress/wp-admin/" ]
 then
 	mkdir -p /var/www/wordpress/
 	cd /var/www/wordpress
-	wp core --allow-root download --version=6.0.2 --locale=ko_KR
+
+	# wp-cli 명령어로 wordpress 다운
+	wp --allow-root core download --version=6.0.2 --locale=ko_KR
+
+	# mariadb와의 연결을 위한 wp-config.php파일 생성
 	wp --allow-root core config --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST
-	wp --allow-root core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL
-	wp  --allow-root user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PW
+
+	# 워드프레스가 이미 설치되어 있으면 설치하지 않고 유저추가도 하지 않음
+	if ! wp --allow-root core is-installed;
+	then
+		# wp-cli 명령어로 wordpress 설치, admin 생성
+		wp --allow-root core install --url=localhost --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL
+
+		# wp-cli 명령어로 사용자 추가 및 권한 부여
+		wp  --allow-root user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PW
+	fi
 fi
 
+# php-fpm 데몬으로 실행
 php-fpm7.3 -F
