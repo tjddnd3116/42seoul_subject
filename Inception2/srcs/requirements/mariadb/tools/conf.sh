95d1af3374a40c81479afb118d11b3b8461b09db
tjddnd3116 <tjddnd3116@gmail.com>
2022-09-15 00:16:37 +0900

    fix :	1. docker-compose.yml 	    restart: unless-stopped -> on-failure 	2. mariadb/Dockerfile 	    mariadb.sh -> mariadb_conf.sh 	3. mariadb/tools/conf.sh 	   GRANT ALL PRIVILEGES ON *.* 	   -> GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.*
    1. if exit status of Container is not 0 then restart
    2. rename shell script for apply mariadb configuration
    3. mysql user can access only wordpress database

--- Inception2/srcs/requirements/mariadb/tools/conf.sh
+++ Inception2/srcs/requirements/mariadb/tools/conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 perl -p -i -e '$.==19 and print "port = 3306\n"' /etc/mysql/mariadb.conf.d/50-server.cnf
-perl -pi -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+perl -p -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 echo "CREATE DATABASE $MYSQL_DATABASE;\
-		GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'wordpress.srcs_inception_net' IDENTIFIED BY '$MYSQL_PASSWORD';\
+		GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'wordpress.%' IDENTIFIED BY '$MYSQL_PASSWORD';\
         FLUSH PRIVILEGES;\
-		ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" > mariadb.sh
+		ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" > mariadb_conf.sh
